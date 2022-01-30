fc19ff8e71681216365446de905e335311116cdb
Princess Flutterheart <akidragon@riseup.net>
2022-01-30 03:06:14 +0000

    Add new file

---
+++ functions/misc.c
@@ -0,0 +1,25 @@
+/* Copyright (C) 2022 PrincessAlicorn*/
+
+#include "../lib/misc.h"
+
+int check_for_root() {
+  uid_t uid = getuid();
+  if (uid == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int check_for_sysd() {
+  FILE *init_daemon = fopen("/proc/1/status", "r");
+  if (init_daemon == NULL) { //if you do not have /proc, you are not on linux, and thus, likely do not have systemd!
+      return 0;
+  }
+  char daemon_name[32];
+  fgets(daemon_name, 32, init_daemon);
+  if (daemon_name[6] == 's' && daemon_name[7] == 'y' && daemon_name[12] == 'd') {
+      return 1;
+  }
+  return 0;
+}
