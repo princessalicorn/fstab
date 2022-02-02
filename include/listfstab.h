c20076bce3973f45fd5de889178163aef3343cdb
Princess Celestia <akidragon@riseup.net>
2022-02-02 16:14:44 -0500

    Update dir name to fit with naming conventions

---
+++ include/listfstab.h
@@ -0,0 +1,7 @@
+/* Copyright (C) 2022 PrincessAlicorn*/
+
+#include<stdio.h>
+
+FILE *open_fstab();
+void list_fstab_files();
+void close_file(FILE *fp);
---
+++ include/proginfo.h
@@ -0,0 +1,4 @@
+/* Copyright (C) 2022 PrincessAlicorn*/
+
+void help_statement();
+void program_information();
